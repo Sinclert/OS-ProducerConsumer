d3836482587d0db7e1bc1bc7b991efc330836879
Sinclert <Sinclert@hotmail.com>
2015-04-22 12:31:11 +0200

    Tenemos que revisar los pthread_exit()

--- factory.c
+++ factory.c
@@ -45,134 +45,127 @@ int received_elements = 0;
 
 int main(int argc, char ** argv){
 
-  int data[3];
-  data[0] = 1;
-  data[1] = 1;
-  data[2] = 2;
-  inserter(data);
-  transporter();
-  receiver();
+    if (argc == 2){
+		    init_factory(argv[1]);
+		    close_factory();
+    }
 
- //  if (argc == 2){
-	// 	init_factory(argv[1]);
-	// 	close_factory();
-	// }
-
- //  else {
-	// 	perror("Invalid syntax: ./factory input_file ");
-	// 	return 0;
-	// }
+    else {
+		    perror("Invalid syntax: ./factory input_file");
+		    return 0;
+    }
 	
-  exit(0);
+    exit(0);
 }
 
 
 
 /* Function that parses the input file and initializes all structures and resources */
 int init_factory(char *file){
-  int i = 0;
-  int error = 0;
 
-  int * number_elements;
-  int * number_modified_elements;
-  int * number_modified_stock;
+    int i = 0;
+    int error = 0;
 
-
-  /* DOUBLE POINTER NEEDED TO PASS AN ONLY ARGUMENT TO THE INSERTION THREADS */
-  int ** insertion_args = NULL;
+    int * number_elements;
+    int * number_modified_elements;
+    int * number_modified_stock;
 
 
-  if (file != NULL){
+    /* DOUBLE POINTER NEEDED TO PASS AN ONLY ARGUMENT TO THE INSERTION THREADS */
+    int ** insertion_args = NULL;
 
-    FILE * filefd = fopen(file, "r");
 
-    error = fscanf(filefd, "%d", &number_inserters);
-    printf("Number inserters %d\n", number_inserters);
-    if (error != 1){
-      perror("Error reading from file\n");
+    if (file != NULL){
+
+        FILE * filefd = fopen(file, "r");
+        error = fscanf(filefd, "%d", &number_inserters);
+        printf("Number inserters %d\n", number_inserters);
+
+        if (error != 1){
+            perror("Error reading from file\n");
+        }
+
+        number_transporters = 1;
+        error = fscanf(filefd, "%d", &number_receivers);
+        printf("Number receivers %d\n", number_receivers);
+
+        if (error != 1){
+            perror("Error reading from file\n");
+        }
+
+
+        /* Allocation of memory */
+
+        // Number of elements that are going to be inserted by each thread
+        number_elements = (int *) malloc(sizeof(int)*number_inserters);
+        // Number of elements which stock is going to be updated
+        number_modified_elements = (int *) malloc(sizeof(int)*number_inserters);
+        // Number of elements that are going to be added to the stock
+        number_modified_stock = (int *) malloc(sizeof(int)*number_inserters);
+        // Pthread_t of each inserter
+        inserters = (pthread_t *) malloc(sizeof(pthread_t)*number_inserters);
+        // Pthread_t of each transporter
+        transporters = (pthread_t *) malloc(sizeof(pthread_t)*number_transporters);
+        // Pthread_t of each receiver
+        receivers = (pthread_t *) malloc(sizeof(pthread_t)*number_receivers);
+
+        // Reading elements depending on number of inserters
+        for (i=0 ; i< number_inserters ; i++){
+            error = fscanf(filefd, "%d %d %d", &number_elements[i], &number_modified_elements[i], &number_modified_stock[i]);
+
+            if (error != 3 || number_modified_elements[i] > number_elements[i]){
+                perror("Error reading from file\n");
+                exit(-1);
+            }
+        }
+
+        // Total number of elements that will be in the database
+        for (i=0 ; i<number_inserters ; i++){
+            total_number += number_elements[i] + (number_modified_elements[i]*number_modified_stock[i]);
+        }
+
+
+        // Init the database
+        error = db_factory_init();	
+        if (error != 0){
+	           perror("Error when initializing the database\n");
+        }
+
+        printf("Total number of elements %d\n", total_number);
+
+
+
+        /* CREATION OF THE THREADS */
+        int j = 0;
+
+        // Inserter threads creation
+        while (j < number_inserters){
+
+            // Each row of the "insertion_args" matrix stores the elements to create, modify and the stock to add of each thread
+            insertion_args[j][0] = number_elements[j];
+            insertion_args[j][1] = number_modified_elements[j];
+            insertion_args[j][2] = number_modified_stock[j];
+
+            pthread_create ((j*sizeof(pthread_t) + inserters), NULL, (void*) inserter, &insertion_args[j]);
+            j++;
+        }
+
+        // Transporter thread creation
+        pthread_create (transporters, NULL, (void*) transporter, NULL);
+        j = 0;
+
+        // Receiver threads creation
+        while (j < number_receivers){
+            pthread_create ((j*sizeof(pthread_t) + receivers), NULL, (void*) receiver, NULL);
+            j++;
+        }
+
+        return 0;
     }
 
-    number_transporters = 1;
-    error = fscanf(filefd, "%d", &number_receivers);
-    printf("Number receivers %d\n", number_receivers);
-
-    if (error != 1){
-      perror("Error reading from file\n");
+    else {
+        return -1;
     }
-
-
-    /* Allocation of memory */
-
-    // Number of elements that are going to be inserted by each thread
-    number_elements = (int *) malloc(sizeof(int)*number_inserters);
-    // Number of elements which stock is going to be updated
-    number_modified_elements = (int *) malloc(sizeof(int)*number_inserters);
-    // Number of elements that are going to be added to the stock
-    number_modified_stock = (int *) malloc(sizeof(int)*number_inserters);
-    // Pthread_t of each inserter
-    inserters = (pthread_t *) malloc(sizeof(pthread_t)*number_inserters);
-    // Pthread_t of each transporter
-    transporters = (pthread_t *) malloc(sizeof(pthread_t)*number_transporters);
-    // Pthread_t of each receiver
-    receivers = (pthread_t *) malloc(sizeof(pthread_t)*number_receivers);
-
-    // Reading elements depending on number of inserters
-    for (i=0 ; i< number_inserters ; i++){
-      error = fscanf(filefd, "%d %d %d", &number_elements[i], &number_modified_elements[i], &number_modified_stock[i]);
-
-      if (error != 3 || number_modified_elements[i] > number_elements[i]){
-        perror("Error reading from file\n");
-        exit(-1);
-      }
-    }
-
-    // Total number of elements that will be in the database
-    for (i=0 ; i<number_inserters ; i++){
-      total_number += number_elements[i] + (number_modified_elements[i]*number_modified_stock[i]);
-    }
-
-
-    // Init the database
-    error = db_factory_init();	
-    if (error != 0){
-	     perror("Error when initializing the database\n");
-    }
-
-    printf("Total number of elements %d\n", total_number);
-
-
-
-    /* CREATION OF THE THREADS */
-    int j = 0;
-
-    // Inserter threads creation
-    while (j < number_inserters){
-
-      // Each row of the "insertion_args" matrix stores the elements to create, modify and the stock to add of each thread
-      insertion_args[j][0] = number_elements[j];
-      insertion_args[j][1] = number_modified_elements[j];
-      insertion_args[j][2] = number_modified_stock[j];
-
-      pthread_create ((j*sizeof(pthread_t) + inserters), NULL, (void*) inserter, &insertion_args[j]);
-      j++;
-    }
-
-    // Transporter thread creation
-    pthread_create (transporters, NULL, (void*) transporter, NULL);
-    j = 0;
-
-    // Receiver threads creation
-    while (j < number_receivers){
-      pthread_create ((j*sizeof(pthread_t) + receivers), NULL, (void*) receiver, NULL);
-      j++;
-    }
-
-    return 0;
-  }
-
-  else {
-    return -1;
-  }
 }
 
 
@@ -180,35 +173,35 @@ int init_factory(char *file){
 /* Function that closes and free the resources used by the factory */
 int close_factory(){
 
-  int i = 0;
-  int error = 0;
+    int i = 0;
+    int error = 0;
 
-  // Close the database
-  error = db_factory_close();
-  if (error != 0){
-      perror("Error when closing the database\n");
-  }
+    // Close the database
+    error = db_factory_close();
+    if (error != 0){
+        perror("Error when closing the database\n");
+    }
 
 
-  /* FINALIZATION OF THE THREADS */
+    /* FINALIZATION OF THE THREADS */
 
-  // Closing inserters thread
-  while (i < number_inserters){
-      pthread_join ((pthread_t)(i*sizeof(pthread_t) + inserters), NULL);
-      i++;
-  }
+    // Closing inserters thread
+    while (i < number_inserters){
+        pthread_join ((pthread_t)(i*sizeof(pthread_t) + inserters), NULL);
+        i++;
+    }
 
-  // Closing transporter thread
-  pthread_join ((pthread_t)transporters, NULL);
+    // Closing transporter thread
+    pthread_join ((pthread_t)transporters, NULL);
 
-  i = 0;
-  // Closing receivers thread
-  while (i < number_receivers){
-      pthread_join ((pthread_t)(i*sizeof(pthread_t) + receivers), NULL);
-      i++;
-  }
+    i = 0;
+    // Closing receivers thread
+    while (i < number_receivers){
+        pthread_join ((pthread_t)(i*sizeof(pthread_t) + receivers), NULL);
+        i++;
+    }
 
-  return 0;
+    return 0;
 }
 
 
@@ -218,42 +211,42 @@ int close_factory(){
 /* Function executed by the inserter thread which is in charge of inserting and updating */
 void * inserter(void * data){
 
-  /* To be completed for concurrency */
-  int ID, stock;
-  int i = 0;
-  int error = 0;
+    /* To be completed for concurrency */
+    int ID, stock;
+    int i = 0;
+    int error = 0;
 
-  // Creation of the elements
-  while (i < data[0]){
-    error = db_factory_create_element("Element", 1, &ID);
+    // Creation of the elements
+    while (i < data[0]){
+        error = db_factory_create_element("Element", 1, &ID);
 
-    if (error != 0){
-      perror("Error when creating the elements");
-      return -1;
-    }
+        if (error != 0){
+            perror("Error when creating the elements");
+            pthread_exit(-1);
+        }
 
-    // If the "i" element needs to be updated, the stock passed as argument in "data[2]" is added
-    if (i < data[1]){
-      error = db_factory_get_stock(ID, &stock);
+        // If the "i" element needs to be updated, the stock passed as argument in "data[2]" is added
+        if (i < data[1]){
+            error = db_factory_get_stock(ID, &stock);
 
-      if (error != 0){
-        perror("Error when getting the stock of the elements");
-        return -1;
-      }
+            if (error != 0){
+                perror("Error when getting the stock of the elements");
+                pthread_exit(-1);
+            }
 
-      error = db_factory_update_stock(ID, (stock + data[2]));
+            error = db_factory_update_stock(ID, (stock + data[2]));
 
-      if (error != 0){
-        perror("Error when updating the stock of the elements");
-        return -1;
-      }
-    }
+            if (error != 0){
+                perror("Error when updating the stock of the elements");
+                pthread_exit(-1);
+            }
+        }
     
-    i++;
-  }
+        i++;
+    }
 
-  printf("Exitting inserter thread\n");
-  return 0;
+    printf("Exitting inserter thread\n");
+    pthread_exit(0);
 }
 
 
@@ -272,15 +265,15 @@ void * transporter(void){
 
       if (error != 0){
           perror("Error when checking the state of the elements");
-          return -1;
+          pthread_exit(-1);
       }
 
       if (status == 1){
           error = db_factory_get_stock(ID, &stock);
 
           if (error != 0){
-            perror("Error when getting the stock of the elements");
-            return -1;
+              perror("Error when getting the stock of the elements");
+              pthread_exit(-1);
           }
 
           if (stock > 0){
@@ -288,7 +281,7 @@ void * transporter(void){
 
               if (error != 0){
                   perror("Error when getting the name of the elements");
-                  return -1;
+                  pthread_exit(-1);
               }
 
               if (belt_elements < 7){
@@ -303,7 +296,7 @@ void * transporter(void){
 
                   if (error != 0){
                       perror("Error when updating the stock of the elements");
-                      return -1;
+                      pthread_exit(-1);
                   }
 
                   // To be printed when inserted in the belt
@@ -320,7 +313,7 @@ void * transporter(void){
       ID = (ID+1) % MAX_DATABASE;
   }
 
-  return 0;
+  pthread_exit(0);
 }
 
 
@@ -338,7 +331,7 @@ void * receiver(){
 
       if (error != 0){
           perror("Error when getting the name of the elements");
-          return -1;
+          pthread_exit(-1);
       }
 
       // To be printed when an element is received
@@ -350,5 +343,5 @@ void * receiver(){
       position = (position+1) % 8;
   }
 
-  return 0;
+  pthread_exit(0);
 }
